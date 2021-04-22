dcc654ef0fd82667cd9dce9267c69d9f1adcea5f
Javier Albornoz <javier.albornoz@hpe.com>
2021-04-22 13:18:26 +0000

    Agregué una línea nueva

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <string>
 
+using namespace std;
 int main() {
-  std::cout << "¡Hola mundo!\n";
-  std::cout << "Otra línea de mensaje\n";
+  cout << "¡Hola mundo!\nendl" << "\n" << endl;
+
+  cout << "Otra línea de mensaje\n";
+
+  cout << "Algo";
+
+
+  int val1=10, val2=1;
+  bool res;
+  res = val1 >= val2;
+
+  int base;
+  string unidades;
+
+  cin >> base;
+  // Comentario
+  cout << "Base del triángulo: "
+       << base << " metros";
+
+  
+
 }
